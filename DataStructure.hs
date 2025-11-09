fa2b117766d4695a170d6c3623047070ac116aee
Eduardo Mendes <eduardo.carbonera@pucpr.edu.br>
2025-11-09 12:42:55 -0800

    feat: add serialization

--- DataStructure.hs
+++ DataStructure.hs
@@ -17,7 +17,8 @@ data Item = Item
     nome        :: String,
     quantidade  :: Int,
     categoria   :: String
-  }
+  }deriving (Show, Read)
+
 
 -- Volumetria de testes hardcoded, precisa refatorar para ser dinâmico
 --Inventario funciona associando Chave:Valor, por isso tem ids duplicados, se pensar em SQL fará mais sentido
@@ -29,13 +30,16 @@ myInventory = Map.fromList
 ("003", Item "003" "Bandagem" 5 "Utilitarios") 
 ]
 
+
 -- AcaoLog pode ser qualquer uma das opções. Tipo com múltiplas variantes. Adicionar o Read
 data AcaoLog = Add | Remove | Update | QueryFail
+deriving (Show, Read)
 
 -- Parecido com o ação log, mas esse precisa que a falha devolva uma String.
 data StatusLog 
   = Sucesso
   | Falha String
+  deriving (Show, Read)
   
  -- Gera a data de quando o log ocorreu, a ação derivando do AcaoLog. Detalhes adicionais e o Status também derivando.
 data LogEntry = LogEntry
@@ -44,13 +48,4 @@ data LogEntry = LogEntry
      acao      :: AcaoLog,
      detalhes  :: String,   
      status    :: StatusLog
-  }
-
-
-
-
-
-
-
-
-  
+  } deriving (Show, Read)
