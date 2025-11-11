bce60653a211049ce6d385dd55674e5b5e9aaa81
Angelo Netho <angelonetho@gmail.com>
2025-11-11 19:04:07 -0300

    feat: add option to update item quantity in inventory menu

--- LoopPrincipal.hs
+++ LoopPrincipal.hs
@@ -63,6 +63,30 @@ removeItemIO = do
         )
         (Funcoes.removeItem horario idItem qtd myInventory)
 
+updateItemIO :: IO ()
+updateItemIO = do
+    putStrLn "\n=== Atualizar Quantidade do Item ==="
+    hFlush stdout
+    putStr "ID do Item: "
+    hFlush stdout
+    idItem <- getLine
+
+    putStr "Nova Quantidade: "
+    hFlush stdout
+    qtdStr <- getLine
+
+    let qtd = read qtdStr :: Int
+    horario <- getCurrentTime
+
+    either
+        (\erro -> putStrLn $ "Erro: " ++ erro)
+        (\(novoInventario, logEntry) -> do
+            putStrLn "Item atualizado com sucesso!"
+            print logEntry
+            print novoInventario
+        )
+        (Funcoes.updateQty horario idItem qtd myInventory)
+
 relatorio :: IO ()
 relatorio = putStrLn "Função relatorio ainda não implementada"
 
@@ -70,7 +94,8 @@ execucaoLoop :: String -> IO ()
 execucaoLoop conferiOpcao
     | conferiOpcao == "1" = addItemIO >> menu
     | conferiOpcao == "2" = removeItemIO >> menu
-    | conferiOpcao == "3" = relatorio >> menu
+    | conferiOpcao == "3" = updateItemIO >> menu
+    | conferiOpcao == "4" = relatorio >> menu
     | conferiOpcao == "0" = putStrLn "Saindo do programa..."
     | otherwise = putStrLn "Operacao nao valida!" >> menu
 
@@ -79,7 +104,8 @@ menu = do
     putStrLn "\nEscolha a operacao que sera realizada:"
     putStrLn "1: Adiciona um novo Item"
     putStrLn "2: Remover Item"
-    putStrLn "3: Relatorio"
+    putStrLn "3: Atualizar Quantidade"
+    putStrLn "4: Relatorio"
     putStrLn "0: Sair do Programa"
     opcao <- getLine
     execucaoLoop opcao
