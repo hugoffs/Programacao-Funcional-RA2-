ce4ed35f9aa2f1f894e8b30fea1c54db47dc3b33
Angelo Netho <angelonetho@gmail.com>
2025-11-11 17:37:43 -0300

    feat: add function to remove items from inventory

--- Funcoes.hs
+++ Funcoes.hs
@@ -31,3 +31,35 @@ addItem horario idItem nomeItem qtd categ inventarioArmazenado
             , status = Sucesso
             }
       in Right (novoInventario, logEntry)
+
+removeItem :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
+removeItem horario idItem qtd inventarioArmazenado
+  | qtd <= 0 = 
+      Left "Quantidade deve ser positiva"
+
+  | Map.notMember idItem inventarioArmazenado = 
+      Left "Item com esse ID não existe"
+
+  | qtdAtual < qtd =
+      Left "Estoque insuficiente"
+
+  | otherwise =
+      let itemAtual = inventarioArmazenado Map.! idItem
+          novaQtd = qtdAtual - qtd
+          itemAtualizado = itemAtual { quantidade = novaQtd }
+          novoInventario = if novaQtd == 0
+                           then Map.delete idItem inventarioArmazenado
+                           else Map.insert idItem itemAtualizado inventarioArmazenado
+          logEntry = LogEntry
+            { timestamp = horario
+            , acao = Remove
+            , detalhes = "Item removido: " ++ nome itemAtual ++
+                          " (ID: " ++ idItem ++
+                          ", Qtd removida: " ++ show qtd ++
+                          ", Qtd restante: " ++ show novaQtd ++ ")"
+            , status = Sucesso
+            }
+      in Right (novoInventario, logEntry)
+  where
+    qtdAtual = maybe 0 quantidade (Map.lookup idItem inventarioArmazenado)
+  
