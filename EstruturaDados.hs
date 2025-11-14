381ba319aa15e474312e6bfb75a2096a71d612db
Eduardo Mendes <eduardo.carbonera@pucpr.edu.br>
2025-11-14 01:05:58 -0800

    Docs: improve documentation in EstruturaDados.hs
    Refactor excess comments

--- EstruturaDados.hs
+++ EstruturaDados.hs
@@ -1,16 +1,10 @@
--- deve refatorar para tudo em português
--- Deixar comentários simples que orientem o usuário
 module EstruturaDados where
 
--- está no documento a biblioteca utilizada. A recomendação e prática comum é usar as duas
---Importa tudo do módulo Data.Map. Precisa usar o prefixo Map. para acessar qualquer coisa (Evita conflito com o Prelude)
 import qualified Data.Map as Map
---Importa apenas o tipo Map. Para usar funções, você ainda precisa importar ou usar prefixo
 import Data.Map (Map)
---Para conseguir ver o tempo
 import Data.Time (UTCTime, getCurrentTime)
 
-
+-- Define a estrutura de um item que estará em Inventario
 data Item = Item
   { 
     itemID      :: String,
@@ -19,9 +13,7 @@ data Item = Item
     categoria   :: String
   }deriving (Show, Read)
 
-
--- Volumetria de testes hardcoded, precisa refatorar para ser dinâmico
---Inventario funciona associando Chave:Valor, por isso tem ids duplicados, se pensar em SQL fará mais sentido
+-- Define um 'Inventario' como um alias de tipo para um 'Map'. Que armazena elementos do tipo Item
 type Inventario = Map String Item
 myInventory = Map.fromList
   [ ("001", Item "001" "Espada" 2 "Armas")
@@ -29,16 +21,16 @@ myInventory = Map.fromList
   , ("003", Item "003" "Bandagem" 5 "Utilitarios")
   ]
 
-
--- AcaoLog pode ser qualquer uma das opções. Tipo com múltiplas variantes. Adicionar o Read
+-- Define a estrutura de registro de ações no sistema
 data AcaoLog = Add | Remove | Update | QueryFail
     deriving (Show, Read)
 
--- Parecido com o ação log, mas esse precisa que a falha devolva uma String.
+-- Define a estrutura de sucesso/falha do registo de ações
 data StatusLog 
   = Sucesso
   | Falha String
   deriving (Show, Read)
+
 
  -- Gera a data de quando o log ocorreu, a ação derivando do AcaoLog. Detalhes adicionais e o Status também derivando.
 data LogEntry = LogEntry
