b64a80fbfed12c2d7488f68c4286ce73417b892b
Eduardo Mendes <eduardo.carbonera@pucpr.edu.br>
2025-11-14 06:28:26 -0800

    fix: MyInventory bug

--- EstruturaDados.hs
+++ EstruturaDados.hs
@@ -4,6 +4,7 @@ import qualified Data.Map as Map
 import Data.Map (Map)
 import Data.Time (UTCTime, getCurrentTime)
 
+
 -- Define a estrutura de um item que estará em Inventario
 data Item = Item
   { 
@@ -13,17 +14,15 @@ data Item = Item
     categoria   :: String
   }deriving (Show, Read)
 
+
 -- Define um 'Inventario' como um alias de tipo para um 'Map'. Que armazena elementos do tipo Item
 type Inventario = Map String Item
-myInventory = Map.fromList
-  [ ("001", Item "001" "Espada" 2 "Armas")
-  , ("002", Item "002" "ArcoFlecha" 1 "ArmasDistancia")
-  , ("003", Item "003" "Bandagem" 5 "Utilitarios")
-  ]
+
 
 -- Define a estrutura de registro de ações no sistema
 data AcaoLog = Add | Remove | Update | QueryFail
     deriving (Show, Read)
+
 
 -- Define a estrutura de sucesso/falha do registo de ações
 data StatusLog 
